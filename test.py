f21545dca478b2a2641dc4fc935c0b13698231a5
VoidOG <VoidGod123@outlook.com>
2024-11-19 00:01:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import pymongo
 import logging
 # Logging Setup
